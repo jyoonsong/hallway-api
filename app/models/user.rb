dc8376f5d53599f1bd5c4b90cec867672911eac3
Jaeyoon Song <jyo3on@gmail.com>
2020-05-09 22:11:42 +0900

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+
+  # Waitings stands for users who I am waiting for
+  # waitings = Follow.where(waited_by_id: self.id)
+  has_many :waiting_for, foreign_key: :waiting_for_id
+  has_many :waitings, through: :waiting_for, source: :waited_by
+
+  # Followers stands for users who waits me
+  # waiters = Wait.where(waiting_for_id: self.id)
+  has_many :waited_by, class_name: :Wait, foreign_key: :waited_by_id
+  has_many :waiters, through: :waited_by, source: :waiting_for
+
+  def request_wait(waiting_for)
+    Wait.create(waited_by_id: self.id, waiting_for_id: waiting_for.id)
+  end
 end
