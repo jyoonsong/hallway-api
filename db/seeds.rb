b56d86a20c2740823ab3f168687cf63198b3b2f4
Jaeyoon Song <jyo3on@gmail.com>
2020-05-12 11:53:30 +0900

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,35 +12,51 @@ User.create([
         last_name: "Song",
         affiliation: "SNU",
         email: "song@jaeyoon.io",
-        link: "https://jaeyoon.io"
+        link: "https://jaeyoon.io",
+        password_digest: BCrypt::Password.create('password1')
     },
     {
         first_name: "Thomas",
         last_name: "Malone",
         affiliation: "MIT",
         email: "malone@mit.edu",
-        link: "https://malone.mit.edu"
+        link: "https://malone.mit.edu",
+        password_digest: BCrypt::Password.create('password2')
     },
     {
         first_name: "Juho",
         last_name: "Kim",
         affiliation: "KAIST",
         email: "juhokim@kaist.ac.kr",
-        link: "https://juhokim.com"
+        link: "https://juhokim.com",
+        password_digest: BCrypt::Password.create('password3')
     },
     {
         first_name: "Lily",
         last_name: "Lala",
         affiliation: "somewhere",
         email: "something@email.com",
-        link: "https://zoom.link"
+        link: "https://zoom.link",
+        password_digest: BCrypt::Password.create('password4')
     },
     {
         first_name: "Lele",
         last_name: "Lolo",
         affiliation: "Haha",
         email: "where@email.com",
-        link: "https://zoom.link.to.somewhere"
+        link: "https://zoom.link.to.somewhere",
+        password_digest: BCrypt::Password.create('password5')
     }
 
+])
+
+Wait.create([
+    {
+        waiting_id: 1,
+        waiter_id: 2
+    },
+    {
+        waiting_id: 4,
+        waiter_id: 1
+    }
 ])
