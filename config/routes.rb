32d359862eb8b18ff959180b71078714eaaf6533
Jaeyoon Song <jyo3on@gmail.com>
2020-05-12 18:44:08 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :show, :index]
 
-  scope '/api/v1' do
     resources :users
     resources :waits
     
@@ -12,5 +10,4 @@ Rails.application.routes.draw do
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
     get '/logged_in', to: 'sessions#is_logged_in?'
-  end
 end
