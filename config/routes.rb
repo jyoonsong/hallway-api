76c54ef3aaa3a7be96cbc01ba40e9058b47571f8
Jaeyoon Song <jyo3on@gmail.com>
2020-05-12 18:45:40 +0900

    add namespace in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  scope '/api/v1' do
     resources :users
     resources :waits
     
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
     get '/logged_in', to: 'sessions#is_logged_in?'
+  end
 end
