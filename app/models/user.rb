4230c52bc0faaa923026cc805b18aada2b12ea91
Jaeyoon Song <jyo3on@gmail.com>
2020-05-12 19:32:05 +0900

    comments to explain relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
   # Waitings stands for users who I am waiting for
-  # waitings = Follow.where(wait_id: self.id)
+  # waits = Wait.where(wait_id: self.id)
+  # waitings = User.where
   has_many :waits, class_name: :Wait, foreign_key: :waiting_id
   has_many :waitings, through: :waits, source: :waiter
 
   # Waiters stands for users who waits me
-  # waiters = Wait.where(is_waited_id: self.id)
+  # is_waiteds = Wait.where(is_waited_id: self.id)
+  # waiters = User
   has_many :is_waiteds, class_name: :Wait, foreign_key: :waiter_id #wait_id
   has_many :waiters, through: :is_waiteds, source: :waiting
 
