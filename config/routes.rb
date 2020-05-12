bad3007188c12688ce11034460c429c5507b5da9
Jaeyoon Song <jyo3on@gmail.com>
2020-05-12 11:53:27 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources: :sessions
 
   scope '/api/v1' do
     resources :users
     resources :waits
-    get '/waits/:user_id/:waiting_for_id', to: 'waits#create'
+    
+    post '/waits/:is_waited_id', to: 'waits#create'
+    delete '/waits/:is_waited_id', to: 'waits#destroy'
   end
 end
